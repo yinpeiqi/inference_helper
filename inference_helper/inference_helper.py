df34c273a13f6f49d6bda61c283f879ab40d0799
yinpeiqi <11810305@mail.sustech.edu.cn>
2022-02-05 09:23:47 +0000

    refactor code

--- inference_helper/inference_helper.py
+++ inference_helper/inference_helper.py
@@ -6,7 +6,8 @@ import tqdm
 from .function_generator import FunctionGenerator
 from .utils import get_new_arg_input
 
-class InferenceHelper():
+
+class InferenceHelperBase():
     def __init__(self, module: nn.Module, batch_size, device, num_workers = 4, debug = False):
         # add a '_' in order not crash with the origin one.
         self._batch_size = batch_size
@@ -15,7 +16,7 @@ class InferenceHelper():
         self._function_generator = FunctionGenerator(module, debug)
         self._schema = self._function_generator.get_schema()
         self._funcs = self._function_generator.get_funcs()
-
+    
     def _trace_output_shape(self, arg2val_map):
         ret_shapes = [[] for _ in range(self._schema.layers_count)]
         for layer, func in zip(self._schema.layers, self._funcs):
@@ -34,7 +35,41 @@ class InferenceHelper():
                 ret_shapes[layer.id].append(val.size()[1:])
         return ret_shapes
 
-    # override here
+    def compute(self, inference_graph, ret_shapes, arg2val_map, layer, func):
+        raise NotImplementedError()
+
+    def inference(self, inference_graph, *args):
+        first_layer_inputs = (inference_graph,) + tuple(args)
+        if len(first_layer_inputs) != len(self._schema.first_layer_input):
+            raise Exception("layer's input not match with args.")
+        arg2val_map = {}
+        for val, arg_name in zip(first_layer_inputs, self._schema.first_layer_input):
+            arg_node = self._schema.name2arg_map[arg_name]
+            arg2val_map[arg_node] = val
+        ret_shapes = self._trace_output_shape(arg2val_map)
+
+        for layer, func in zip(self._schema.layers, self._funcs):
+            
+            rets = self.compute(inference_graph, ret_shapes, arg2val_map, layer, func)
+
+            # delete intermediate val
+            for arg_node in layer.inputs:
+                if arg_node.input_layers[-1] == layer and arg_node.input_layers[0] != self._schema.get_layer(0):
+                    del arg2val_map[arg_node]
+
+            for ret, arg_node in zip(rets, layer.outputs):
+                arg2val_map[arg_node] = ret
+
+        outputs = ()
+        for name in self._schema.last_layer_output:
+            arg_node = self._schema.name2arg_map[name]
+            outputs += (arg2val_map[arg_node],)
+        if len(outputs) == 1:
+            return outputs[0]
+        return tuple(outputs)
+
+
+class InferenceHelper(InferenceHelperBase):
     def compute(self, inference_graph, ret_shapes, arg2val_map, layer, func):
         sampler = dgl.dataloading.MultiLayerFullNeighborSampler(1)
         dataloader = dgl.dataloading.NodeDataLoader(
@@ -70,33 +105,3 @@ class InferenceHelper():
                     else:
                         raise RuntimeError("Can't determine return's type.")
         return rets
-
-    def inference(self, inference_graph, *args):
-        first_layer_inputs = (inference_graph,) + tuple(args)
-        if len(first_layer_inputs) != len(self._schema.first_layer_input):
-            raise Exception("layer's input not match with args.")
-        arg2val_map = {}
-        for val, arg_name in zip(first_layer_inputs, self._schema.first_layer_input):
-            arg_node = self._schema.name2arg_map[arg_name]
-            arg2val_map[arg_node] = val
-        ret_shapes = self._trace_output_shape(arg2val_map)
-
-        for layer, func in zip(self._schema.layers, self._funcs):
-            
-            rets = self.compute(inference_graph, ret_shapes, arg2val_map, layer, func)
-
-            # delete intermediate val
-            for arg_node in layer.inputs:
-                if arg_node.input_layers[-1] == layer and arg_node.input_layers[0] != self._schema.get_layer(0):
-                    del arg2val_map[arg_node]
-
-            for ret, arg_node in zip(rets, layer.outputs):
-                arg2val_map[arg_node] = ret
-
-        outputs = ()
-        for name in self._schema.last_layer_output:
-            arg_node = self._schema.name2arg_map[name]
-            outputs += (arg2val_map[arg_node],)
-        if len(outputs) == 1:
-            return outputs[0]
-        return tuple(outputs)
