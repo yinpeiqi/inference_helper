92c28226df815965f21e72e3f02e808206a08897
yinpeiqi <11810305@mail.sustech.edu.cn>
2022-02-27 09:33:27 +0000

    add pertery

--- inference_helper/tracer.py
+++ inference_helper/tracer.py
@@ -1,8 +1,11 @@
 import math
+import operator
+import builtins
 
 import torch
 import dgl.nn
 from torch.fx import Tracer, Proxy, Node, GraphModule
+from torch.fx.proxy import Attribute
 from torch.fx._compatibility import compatibility
 from dgl.nn.functional import edge_softmax
 from dgl.function.message import BinaryMessageFunction, CopyMessageFunction
@@ -50,10 +53,18 @@ class DGLTracer(Tracer):
     @compatibility(is_backward_compatible=True)
     def proxy(self, node: Node) -> "Proxy":
         if self.graph_proxy is None:
-            return DGLGraphProxy(node, self)
+            return self.dgl_graph_proxy(node)
         if is_dgl_function(node.target):
             return DGLFunctionProxy(node, self)
         return Proxy(node, self)
+
+    @compatibility(is_backward_compatible=True)
+    def dgl_graph_proxy(self, node: Node) -> "Proxy":
+        return DGLGraphProxy(node, self)
+
+    @compatibility(is_backward_compatible=True)
+    def dgl_graph_attribute(self, node: Node) -> "Proxy":
+        return DGLGraphAttribute(node, self)
 
     @compatibility(is_backward_compatible=True)
     def create_arg(self, a):
@@ -65,21 +76,68 @@ class DGLTracer(Tracer):
             return super().create_arg(a)
 
     def is_leaf_module(self, m: torch.nn.Module, module_qualified_name : str):
-        if m.__class__.__name__ in self.conv_modules:
-            return True
+        # if m.__class__.__name__ in self.conv_modules:
+        #     return True
         return super().is_leaf_module(m, module_qualified_name)
 
 
 class DGLGraphProxy(Proxy):
+    def __init__(self, node: Node, tracer: Tracer = None):
+        super().__init__(node, tracer)
+        node.node_type = "DGLGraph"
+
     @property
     def is_block(self):
-        # TODO
         return True
+
+    def local_var(self):
+        return self.tracer.create_proxy("call_method", "local_var", (self,), {}, 
+            proxy_factory_fn=self.tracer.dgl_graph_proxy)
+
+    def __getitem__(self, rhs):
+        return self.tracer.create_proxy("call_function", operator.getitem, (self, rhs), {}, 
+            proxy_factory_fn=self.tracer.dgl_graph_proxy)
+
+    # def apply_edges(self, *args, **kwargs):
+    #     print(*args, **kwargs)
+
+    @property
+    def srcdata(self):
+        return self.tracer.create_proxy("call_function", builtins.getattr, (self, "srcdata"), {},
+            proxy_factory_fn=self.tracer.dgl_graph_attribute)
+
+    @property
+    def dstdata(self):
+        return self.tracer.create_proxy("call_function", builtins.getattr, (self, "dstdata"), {},
+            proxy_factory_fn=self.tracer.dgl_graph_attribute)
+
+    @property
+    def ndata(self):
+        return self.tracer.create_proxy("call_function", builtins.getattr, (self, "ndata"), {},
+            proxy_factory_fn=self.tracer.dgl_graph_attribute)
+
+    @property
+    def edata(self):
+        return self.tracer.create_proxy("call_function", builtins.getattr, (self, "edata"), {},
+            proxy_factory_fn=self.tracer.dgl_graph_attribute)
 
     def __str__(self):
         return "Graph{}".format(super().__str__())
 
+
+class DGLGraphAttribute(Proxy):
+    @compatibility(is_backward_compatible=True)
+    def __init__(self, node: Node, tracer: Tracer = None):
+        super().__init__(node, tracer)
+        node.node_type = "DGLGraphAttribute"
+
+
 class DGLFunctionProxy(Proxy):
+    @compatibility(is_backward_compatible=True)
+    def __init__(self, node: Node, tracer: Tracer = None):
+        super().__init__(node, tracer)
+        node.node_type = "DGLFunction"
+
     def __str__(self):
         return "DGLFunction{}".format(super().__str__())
 
