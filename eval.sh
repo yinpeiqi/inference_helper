557eb9f772e6a771456d29df83eca4ab405092af
yinpeiqi <11810305@mail.sustech.edu.cn>
2022-05-05 02:32:28 +0000

    modify bash

--- eval.sh
+++ eval.sh
@@ -1,26 +1,34 @@
 
 layer=3
 hidden=128
+rates=(0.7 0.9 0.9)
 models=(GAT GCN JKNET)
 datasets=(ogbn-products friendster ogbn-papers100M)
-best_batches=(250000 20000 60000 500000 60000 250000 280000 15000 40000)
+
+best_ran_batches=(250000 20000 60000 500000 60000 250000 280000 30000 40000)
+best_rcmk_batches=(250000 10000 20000 500000 40000 130000 280000 8000 25000)
 
 for i in {0..2}
     do
     for j in {0..2}
         do
+        rate=${rates[i]}
         model=${models[i]}
         dataset=${datasets[j]}
-        batch_size=${best_batches[i*3+j]}
+        ran_batch_size=${best_ran_batches[i*3+j]}
+        rcmk_batch_size=${best_rcmk_batches[i*3+j]}
 
         # auto solotion
-        python -u exp/baseline/run.py --model $model --num-layers $layer --num-hidden $hidden --num-heads 2 --use-uva --dataset $dataset --auto
+        python -u exp/baseline/run.py --model $model --num-layers $layer --num-hidden $hidden --num-heads 2 --use-uva --dataset $dataset --auto --free-rate $rate
 
         # auto solotion with reorder
-        python -u exp/baseline/run.py --model $model --num-layers $layer --num-hidden $hidden --num-heads 2 --use-uva --dataset $dataset --auto --reorder
+        python -u exp/baseline/run.py --model $model --num-layers $layer --num-hidden $hidden --num-heads 2 --use-uva --dataset $dataset --auto --reorder --free-rate $rate
 
         # best batch baseline
-        python -u exp/baseline/run.py --model $model --num-layers $layer --num-hidden $hidden --num-heads 2 --use-uva --dataset $dataset --batch-size $batch_size
+        python -u exp/baseline/run.py --model $model --num-layers $layer --num-hidden $hidden --num-heads 2 --use-uva --dataset $dataset --batch-size $ran_batch_size
+
+        # best batch baseline with reorder
+        python -u exp/baseline/run.py --model $model --num-layers $layer --num-hidden $hidden --num-heads 2 --use-uva --dataset $dataset --batch-size $rcmk_batch_size --reorder
 
     done
 done
