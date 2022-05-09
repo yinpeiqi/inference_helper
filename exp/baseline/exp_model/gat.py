694779968ae2528b19f8ecefa54237888d08c62c
yinpeiqi <11810305@mail.sustech.edu.cn>
2022-05-09 07:19:20 +0000

    add inference auto

--- exp/baseline/exp_model/gat.py
+++ exp/baseline/exp_model/gat.py
@@ -2,6 +2,7 @@ import torch.fx
 import torch as th
 import torch.nn as nn
 import gc
+import numpy as np
 
 import dgl
 from dgl.nn import GATConv
@@ -9,6 +10,8 @@ from inference_helper.profiler import Profiler
 import tqdm
 from dgl.utils import pin_memory_inplace, unpin_memory_inplace, gather_pinned_tensor_rows
 from inference_helper.utils import update_out_in_chunks
+from inference_helper.custom_dataloader import CustomDataloader
+from inference_helper.auto_tuner import get_auto_tuner
 
 class GAT(nn.Module):
     def __init__(self,
@@ -125,4 +128,98 @@ class GAT(nn.Module):
             # print(memorys)
             profiler.show()
         # print("memory: ", torch.cuda.max_memory_allocated() // 1024 ** 2)
+        return y
+
+
+    def inference_auto(self, g, device, x, nids, use_uva = False, free_rate=0.9):
+        for k in list(g.ndata.keys()):
+            g.ndata.pop(k)
+        for k in list(g.edata.keys()):
+            g.edata.pop(k)
+        
+        in_degrees = g.in_degrees(nids).numpy()
+        prefix_sum_in_degrees = [0]
+        prefix_sum_in_degrees.extend(np.cumsum(in_degrees).tolist())
+        prefix_sum_in_degrees.append(2e18)
+        torch.cuda.reset_peak_memory_stats()
+        
+        for l, layer in enumerate(self.gat_layers):
+            gc.collect()
+            th.cuda.empty_cache()
+
+            auto_tuner = get_auto_tuner(device)
+            start_max_node = 2000
+            start_max_edge = 500000
+
+            if l != self.num_layers - 1:
+                y = th.zeros(g.number_of_nodes(), self.heads[l] * self.hidden_features)
+            else:
+                y = th.zeros(g.number_of_nodes(), self.out_features)
+
+            if use_uva:
+                pin_memory_inplace(x)
+                nids = nids.to(device)
+
+            sampler = dgl.dataloading.MultiLayerFullNeighborSampler(1)
+            dataloader = CustomDataloader(
+                g,
+                nids,
+                sampler,
+                start_max_node,
+                start_max_edge,
+                prefix_sum_in_degrees,
+                device=device,
+                use_uva=use_uva,
+                shuffle=False)
+            
+            profiler = Profiler()
+            profiler.record_and_reset()
+            for input_nodes, output_nodes, blocks in dataloader:
+                try:
+                    auto_tuner.reset_state()
+                    torch.cuda.empty_cache()
+                    auto_tuner.set_free(free_rate)
+
+                    profiler.tag()
+                    profiler.record_name("total input nodes", input_nodes.shape[0])
+                    block = blocks[0].to(device)
+                    if use_uva:
+                        h = gather_pinned_tensor_rows(x, input_nodes)
+                    else:
+                        h = x[input_nodes].to(device)
+                    profiler.tag()
+
+                    h = layer(block, h)
+                    if l == self.num_layers - 1:
+                        logits = h.mean(1)
+                        profiler.tag()
+                        y[output_nodes] = logits.cpu()
+                    else:
+                        h = h.flatten(1)
+                        profiler.tag()
+                        update_out_in_chunks(y, output_nodes, h)
+                    profiler.tag()
+
+                    auto_tuner.set_max()
+                    print(block, torch.cuda.max_memory_allocated() // 1024 ** 2)
+                    nxt_max_node, nxt_max_edge = auto_tuner.search(blocks[0])
+                
+                except Exception as e:
+                    print(e)
+                    profiler.tag()
+                    nxt_max_node, nxt_max_edge = auto_tuner.break_peak(blocks[0])
+                    dataloader.reset_batch_node(output_nodes.shape[0])
+                    gc.collect()
+
+                finally:
+                    dataloader.modify_max_node(nxt_max_node)
+                    dataloader.modify_max_edge(nxt_max_edge)
+                    torch.cuda.empty_cache()
+                    torch.cuda.reset_peak_memory_stats()
+                    profiler.record_and_reset()
+
+            if use_uva:
+                unpin_memory_inplace(x)
+            x = y
+            profiler.show()
         return y
