05b05ab42865312776cab2e583e7d6edd88e9f48
yinpeiqi <11810305@mail.sustech.edu.cn>
2022-05-14 10:27:21 +0000

    modify download

--- download_data.sh
+++ download_data.sh
@@ -5,8 +5,9 @@ python exp/baseline/run.py --dataset ogbn-products --load-data
 python exp/baseline/run.py --dataset ogbn-papers100M --load-data --reorder
 python exp/baseline/run.py --dataset ogbn-papers100M --load-data
 
-mkdir ../dataset
-cd ../dataset
+cd ..
+mkdir dataset
+cd dataset
 wget https://snap.stanford.edu/data/bigdata/communities/com-friendster.ungraph.txt.gz
 gunzip https://snap.stanford.edu/data/bigdata/communities/com-friendster.ungraph.txt.gz
 cd ../inference_helper
