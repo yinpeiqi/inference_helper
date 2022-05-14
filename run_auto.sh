be2a0440613f0eededd8505495574536631dc1af
yinpeiqi <11810305@mail.sustech.edu.cn>
2022-05-14 15:00:53 +0000

    modify bash

--- run_auto.sh
+++ run_auto.sh
@@ -1,11 +1,14 @@
-python -u exp/baseline/run.py --dataset ogbn-products --model GCN --reorder --ssd --auto
-python -u exp/baseline/run.py --dataset ogbn-products --model GAT --reorder --ssd --auto
-python -u exp/baseline/run.py --dataset ogbn-products --model JKNET --reorder --ssd --auto
+for rate in 0.8 0.85 0.9;
+do
+    python -u exp/baseline/run.py --dataset ogbn-products --model GCN --reorder --ssd --auto --free-rate $rate
+    python -u exp/baseline/run.py --dataset ogbn-products --model GAT --reorder --ssd --auto --free-rate $rate
+    python -u exp/baseline/run.py --dataset ogbn-products --model JKNET --reorder --ssd --auto --free-rate $rate
 
-python -u exp/baseline/run.py --dataset friendster --model GCN --reorder --ssd --auto
-python -u exp/baseline/run.py --dataset friendster --model GAT --reorder --ssd --auto
-python -u exp/baseline/run.py --dataset friendster --model JKNET --reorder --ssd --auto
+    python -u exp/baseline/run.py --dataset friendster --model GCN --reorder --ssd --auto --free-rate $rate
+    python -u exp/baseline/run.py --dataset friendster --model GAT --reorder --ssd --auto --free-rate $rate
+    python -u exp/baseline/run.py --dataset friendster --model JKNET --reorder --ssd --auto --free-rate $rate
 
-python -u exp/baseline/run.py --dataset ogbn-papers100M --model GCN --reorder --ssd --auto
-python -u exp/baseline/run.py --dataset ogbn-papers100M --model GAT --reorder --ssd --auto
-python -u exp/baseline/run.py --dataset ogbn-papers100M --model JKNET --reorder --ssd --auto
+    python -u exp/baseline/run.py --dataset ogbn-papers100M --model GCN --reorder --ssd --auto --free-rate $rate
+    python -u exp/baseline/run.py --dataset ogbn-papers100M --model GAT --reorder --ssd --auto --free-rate $rate
+    python -u exp/baseline/run.py --dataset ogbn-papers100M --model JKNET --reorder --ssd --auto --free-rate $rate
+done
