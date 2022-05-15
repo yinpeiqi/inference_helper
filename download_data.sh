3164634ae694a4d0da946f6d4ace06f0cb2f74c2
yinpeiqi <11810305@mail.sustech.edu.cn>
2022-05-15 02:53:45 +0000

    modify bash

--- download_data.sh
+++ download_data.sh
@@ -1,18 +1,21 @@
 python create_feature_in_ssd.py
+echo "create feature done!"
 
 python exp/baseline/run.py --dataset ogbn-products --load-data --reorder
 python exp/baseline/run.py --dataset ogbn-products --load-data
-
+echo "finished ogbn-products"
 
 python exp/baseline/run.py --dataset ogbn-papers100M --load-data --reorder
 python exp/baseline/run.py --dataset ogbn-papers100M --load-data
+echo "finished ogbn-papers100M"
 
 cd ..
 mkdir dataset
 cd dataset
 wget https://snap.stanford.edu/data/bigdata/communities/com-friendster.ungraph.txt.gz
-gunzip https://snap.stanford.edu/data/bigdata/communities/com-friendster.ungraph.txt.gz
+gunzip com-friendster.ungraph.txt.gz
 cd ../inference_helper
 
 python exp/baseline/run.py --dataset friendster --load-data --reorder
 python exp/baseline/run.py --dataset friendster --load-data
+echo "finished friendster"
