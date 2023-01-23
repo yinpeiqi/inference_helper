a41c4fdf847c71027af60f5f293aa5677480e8c7
yinpeiqi <11810305@mail.sustech.edu.cn>
2023-01-23 15:36:21 +0000

    add

--- run_manual.sh
+++ run_manual.sh
@@ -17,15 +17,17 @@ python3 -u exp/baseline/run.py --model JKNET --use-uva --dataset ogbn-products -
 # Table 3
 python3 -u exp/baseline/run.py --model GCN --use-uva --dataset ogbn-products --batch-size 200000 --fan-out --num-layers 3
 python3 -u exp/baseline/run.py --model GCN --use-uva --dataset livejournal1 --batch-size 100000 --fan-out --num-layers 3
+python3 -u exp/baseline/run.py --model GCN --use-uva --dataset ogbn-papers100M --batch-size 100000 --fan-out --num-layers 2
+python3 -u exp/baseline/run.py --model GCN --use-uva --dataset friendster --batch-size 10000 --fan-out --num-layers 2
 
 # Table 2
 python3 -u exp/baseline/run.py --model GCN --use-uva --dataset livejournal1 --batch-size 100000 --num-layers 3
 python3 -u exp/baseline/run.py --model GAT --use-uva --dataset livejournal1 --batch-size 100000 --num-layers 3
 python3 -u exp/baseline/run.py --model JKNET --use-uva --dataset livejournal1 --batch-size 100000 --num-layers 2
 
-python3 -u exp/baseline/run.py --model GCN --use-uva --dataset ogbn-papers100M --batch-size 200000 --num-layers 2
-python3 -u exp/baseline/run.py --model GAT --use-uva --dataset ogbn-papers100M --batch-size 200000 --num-layers 2
-python3 -u exp/baseline/run.py --model JKNET --use-uva --dataset ogbn-papers100M --batch-size 200000 --num-layers 1
+python3 -u exp/baseline/run.py --model GCN --use-uva --dataset ogbn-papers100M --batch-size 100000 --num-layers 2
+python3 -u exp/baseline/run.py --model GAT --use-uva --dataset ogbn-papers100M --batch-size 100000 --num-layers 2
+python3 -u exp/baseline/run.py --model JKNET --use-uva --dataset ogbn-papers100M --batch-size 100000 --num-layers 1
 
 python3 -u exp/baseline/run.py --model GCN --use-uva --dataset friendster --batch-size 10000 --num-layers 2
 python3 -u exp/baseline/run.py --model GAT --use-uva --dataset friendster --batch-size 10000 --num-layers 2
